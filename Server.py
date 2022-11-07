9ceb62c194258f0c73b40ca687a508a6f984b8af
Jannek Vieth <jannekvieth@outlook.de>
2022-11-07 15:13:03 +0100

    feat: added function to delete entries in database

--- Server.py
+++ Server.py
@@ -2,13 +2,13 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
-def get_gb_connection():
+def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
 
 def get_post(post_id):
-    conn = get_gb_connection()
+    conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
     conn.close()
@@ -21,7 +21,7 @@ app.config['SECRET KEY'] = 'your secret key'
 
 @app.route('/')
 def index():
-    conn = get_gb_connection()
+    conn = get_db_connection()
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
@@ -40,7 +40,7 @@ def create():
         if not title:
             flash('Title is required')
         else:
-            conn = get_gb_connection()
+            conn = get_db_connection()
             conn.execute('INSERT INTO posts(title, content) VALUES (?, ?)',
                          (title, content))
             conn.commit()
@@ -60,14 +60,24 @@ def edit(id):
         if not title:
             flash('Title is required!')
         else:
-            conn = get_gb_connection()
+            conn = get_db_connection()
             conn.execute('UPDATE posts SET title = ?, content = ?'
                          'WHERE id = ?',
                          (title, content, id))
             conn.commit()
             conn.close()
-            return redirect(url_for(index))
+            return redirect(url_for('index'))
 
     return render_template('edit.html', post=post)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
 
 app.run()
