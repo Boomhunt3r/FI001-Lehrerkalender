ad38a1e30368b9e351d4b683de7396e1d83c0bfe
Jannek Vieth <jannekvieth@outlook.de>
2022-11-08 12:06:18 +0100

    feat(login): added functionality to open index site after login and login site after logout

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from . import db
+from . import Server
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
+@login_required
 def index():
-    return render_template('index.html')
+    Server.index()
 
 @main.route('/profile')
-@login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
