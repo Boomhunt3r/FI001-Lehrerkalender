9aba3fade4efa4be34f499ed29efb2e871ad592b
Jannek Vieth <jannekvieth@outlook.de>
2022-11-09 11:17:01 +0100

    feat: added function to retrieve and send data to database

--- Server.py
+++ Server.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
+import datatime
 
 def get_db_connection():
     conn = sqlite3.connect('lehrerkalender.db')
@@ -83,11 +84,14 @@ def get_login_data(email):
     conn = get_db_connection()
     user = conn.execute('SELECT * FROM Login WHERE Email = ?',
                         (email,)).fetchone()
-
-
     return user
 
-def get_classes():
+def get_all_logins():
+    conn = get_db_connection()
+    users = conn.execute('SELECT * FROM Login').fetchall()
+    return users
+
+def get_all_classes():
     conn = get_db_connection()
     classes = conn.execute('SELECT * FROM Klasse ').fetchall()
     conn.close()
@@ -95,50 +99,60 @@ def get_classes():
 
 def get_class(id):
     conn = get_db_connection()
+    sclass = conn.execute('SELECT * FROM Klasse WHERE Klassenid = ?', (id,)).fetchone()
+    return sclass
 
-    return None
-
-def get_students_in_class(id):
+def get_all_students_in_class(id):
     conn = get_db_connection()
-
-    return None
+    students = conn.execute('SELECT * FROM Schueler WHERE Klasse = ?', (id,)).fetchall()
+    return students
 
 def get_student(id):
     conn = get_db_connection()
+    student = conn.execute('SELECT * FROM Schueler WHERE SchuelerID = ?', (id,)).fetchone()
+    return student
 
-    return None
-
-def get_appointments(id):
+def get_all_appointments():
     conn = get_db_connection()
-
-    return None
+    appointments = conn.execute('SELECT * FROM Termin').fetchall()
+    return appointments
 
 def get_appointment(id):
     conn = get_db_connection()
+    appointment = conn.execute('SELECT * FROM Termine WHERE TerminId = ?', (id,)).fetchone()
+    return appointment
 
-    return None
-
-def set_student(id):
+def get_all_students():
     conn = get_db_connection()
+    students = conn.execute('SELECT * FROM Schueler').fetchall()
+    return students
 
-    return None
-
-def set_class(id):
+def set_student(Vorname, Nachname, Klasse, Klassenlehrer, PLZ, Straße, Hausnummer):
     conn = get_db_connection()
+    conn.execute('INSERT INTO Schueler(Vorname, Nachname, Klasse, Klassenlehrer, PLZ, Straße, Hausnummer) VALUES (?,?,?,?,?,?, ?)',
+                 (Vorname, Nachname, Klasse, Klassenlehrer, PLZ, Straße, Hausnummer))
+    conn.commit()
+    conn.close()
 
-    return None
-
-def set_appointment(id):
+def set_class(Klassenname, Klassenlehrer):
     conn = get_db_connection()
+    conn.execute('INSERT INTO Klasse(Klassenname, Klassenlehrer) VALUES (?, ?)',
+                 (Klassenname, Klassenlehrer))
+    conn.commit()
+    conn.close()
 
-    return None
-
-def set_login_data(id):
+def set_appointment(Titel, Zeitraum, Wochentag, Teilnehmer, Ort, Notiz):
     conn = get_db_connection()
+    conn.execute('INSERT INTO Termin(Titel, erstelltAM, Zeitraum, Wochentag, Teilnehmer, Ort, Notiz) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                 (Titel, datatime.get_time(), Zeitraum, Wochentag, Teilnehmer, Ort, Notiz))
+    conn.commit()
+    conn.close()
 
-    return None
-
-
-print(get_login_data('laqua@gso.schule.koeln'))
+def set_login_data(Email, Password):
+    conn = get_db_connection()
+    conn.execute('INSERT INTO Login(Email, Password) VALUES(?, ?)',
+                 (Email, Password))
+    conn.commit()
+    conn.close()
 
 #app.run()
