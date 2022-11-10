4b2312f94bfff45c3c5be220b3b9ccf651735a00
Jannek <jannekvieth@outlook.de>
2022-11-10 21:45:11 +0100

    nein

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-class User:
+class User():
     def __init__(self, email, password, is_active):
+        self.id = id
         self.email = email
         self.password = password
         self.is_active = is_active
